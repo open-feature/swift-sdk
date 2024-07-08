0654d74f2d20dc64049fceb2d721f73db2ee1950
Nicklas Lundin <nicklasl@spotify.com>
2024-07-08 11:39:15 +0200

    feat: add support for provider fatal error
    Signed-off-by: Nicklas Lundin <nicklasl@spotify.com>

--- Tests/OpenFeatureTests/Helpers/AlwaysBrokenProvider.swift
+++ Tests/OpenFeatureTests/Helpers/AlwaysBrokenProvider.swift
@@ -1,11 +1,12 @@
-import Foundation
 import Combine
+import Foundation
 
 @testable import OpenFeature
 
 class AlwaysBrokenProvider: FeatureProvider {
     var metadata: ProviderMetadata = AlwaysBrokenMetadata()
     var hooks: [any Hook] = []
+    var throwFatal = false
     private let eventHandler = EventHandler()
 
     func onContextSet(oldContext: OpenFeature.EvaluationContext?, newContext: OpenFeature.EvaluationContext) {
@@ -19,30 +20,45 @@ class AlwaysBrokenProvider: FeatureProvider {
     func getBooleanEvaluation(key: String, defaultValue: Bool, context: EvaluationContext?) throws
         -> OpenFeature.ProviderEvaluation<Bool>
     {
+        if self.throwFatal {
+            throw OpenFeatureError.providerFatarError(message: "Always broken")
+        }
         throw OpenFeatureError.flagNotFoundError(key: key)
     }
 
     func getStringEvaluation(key: String, defaultValue: String, context: EvaluationContext?) throws
         -> OpenFeature.ProviderEvaluation<String>
     {
+        if self.throwFatal {
+            throw OpenFeatureError.providerFatarError(message: "Always broken")
+        }
         throw OpenFeatureError.flagNotFoundError(key: key)
     }
 
     func getIntegerEvaluation(key: String, defaultValue: Int64, context: EvaluationContext?) throws
         -> OpenFeature.ProviderEvaluation<Int64>
     {
+        if self.throwFatal {
+            throw OpenFeatureError.providerFatarError(message: "Always broken")
+        }
         throw OpenFeatureError.flagNotFoundError(key: key)
     }
 
     func getDoubleEvaluation(key: String, defaultValue: Double, context: EvaluationContext?) throws
         -> OpenFeature.ProviderEvaluation<Double>
     {
+        if self.throwFatal {
+            throw OpenFeatureError.providerFatarError(message: "Always broken")
+        }
         throw OpenFeatureError.flagNotFoundError(key: key)
     }
 
     func getObjectEvaluation(key: String, defaultValue: OpenFeature.Value, context: EvaluationContext?) throws
         -> OpenFeature.ProviderEvaluation<OpenFeature.Value>
     {
+        if self.throwFatal {
+            throw OpenFeatureError.providerFatarError(message: "Always broken")
+        }
         throw OpenFeatureError.flagNotFoundError(key: key)
     }
 
