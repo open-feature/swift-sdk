12b8c83635f76efad61f8c35c07973d1ce700e9b
jescriba <joshua.escribano@includedhealth.com>
2025-09-12 15:58:44 -0700

    Allow FlagEvaluationDetails to be a public API
    Signed-off-by: jescriba <joshua.escribano@includedhealth.com>

--- Sources/OpenFeature/FlagEvaluationOptions.swift
+++ Sources/OpenFeature/FlagEvaluationOptions.swift
@@ -1,6 +1,15 @@
 import Foundation
 
+/// FlagEvaluationOptions is a struct that enables passing in custom hooks and hints to the flag evaluation process.
 public struct FlagEvaluationOptions {
-    var hooks: [any Hook] = []
-    var hookHints: [String: Any] = [:]
+    public let hooks: [any Hook]
+    public let hookHints: [String: Any]
+
+    public init(
+        hooks: [any Hook] = [],
+        hookHints: [String: Any] = [:]
+    ) {
+        self.hooks = hooks
+        self.hookHints = hookHints
+    }
 }
